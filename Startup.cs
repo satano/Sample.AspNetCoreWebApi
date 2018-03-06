c29ad71a96e31af8caa7cbdfdb10e0b5a8ec7761
Milan Martiniak <mino.martiniak@gmail.com>
2018-03-06 07:20:10 +0100

    Add static files

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Sample.AspNetCoreWebApi.Middlewares;
@@ -28,7 +30,9 @@ namespace Sample.AspNetCoreWebApi
 
             services.AddKorm(Configuration)
                 .AddRepositories()
-                .AddJwtAuthorization(Configuration);
+                .AddJwtAuthorization(Configuration)
+                .AddDirectoryBrowser()
+                .AddResponseCompression();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -38,7 +42,15 @@ namespace Sample.AspNetCoreWebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseAuthentication()
+            app.UseResponseCompression()
+                .UseFileServer(new FileServerOptions
+                {
+                    FileProvider = new PhysicalFileProvider(
+                            Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+                        RequestPath = "/StaticFiles",
+                        EnableDirectoryBrowsing = true
+                })
+                .UseAuthentication()
                 .UseLogging()
                 .UseMvc();
         }
