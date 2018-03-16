3cf38a03ff9dd8c0eb83bb14a5d8acbdb26aedd3
Milan Martiniak <mino.martiniak@gmail.com>
2018-03-16 08:45:27 +0100

    Reorganize folder structure and create test project

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Sample.AspNetCoreWebApi.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
